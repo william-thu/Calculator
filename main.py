d232a3ef117f823b809faf0d133c07e66a465066
william-thu <59815676+william-thu@users.noreply.github.com>
2023-10-31 17:52:23 +0000

    rewrote with function

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 operator_list = ["+", "-", "*", "/"]
 
-while True:
-    operator = input("Choose an operation (+ - * /): ")
-    if operator in operator_list:
-        x = int(input("Enter first number: "))
-        y = int(input("Enter second number: "))
+def calculate(operator, firstnum, secondnum):
+    while True:
         if operator == operator_list[0]:
-            result = x+y
-            print(f"{x} + {y} = {result}")
+            result = firstnum + secondnum
+            print(f"{firstnum} + {secondnum} = {result}")
             break
         elif operator == operator_list[1]:
-            result = x-y
-            print(f"{x} - {y} = {result}")
+            result = firstnum - secondnum
+            print(f"{firstnum} - {secondnum} = {result}")
             break
         elif operator == operator_list[2]:
-            result = x*y
-            print(f"{x} * {y} = {result}")
+            result = firstnum * secondnum
+            print(f"{firstnum} * {secondnum} = {result}")
             break
         elif operator == operator_list[3]:
             try:
-                result = x / y
-                print(f"{x} / {y} = {result}")
+                result = firstnum / secondnum
+                print(f"{firstnum} / {secondnum} = {result}")
                 break
             except ZeroDivisionError:
                 print("Cannot divide by zero.")
                 break
+
+valid_operator = False
+while valid_operator == False:
+    operator = input("Choose an operation (+ - * /): ")
+    if operator in operator_list:
+        valid_operator = True
+        x = int(input("Enter first number: "))
+        y = int(input("Enter second number: "))
+
     else:
         print("Invalid input. Choose an operation (+ - * /): ")
+
+calculate(operator, x, y)
